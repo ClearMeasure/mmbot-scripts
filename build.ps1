5d013063e0cb24809f04df4efe9b5aaa9893ecd7
Andrew Siemer <andrew@clear-measure.com>
2014-12-19 19:45:19 +0000

    adding init build script

---
+++ build.ps1
@@ -0,0 +1,44 @@
+#for teamcity integration to ensure proper output
+trap
+{
+    write-output $_
+    ##teamcity[buildStatus status='FAILURE' ]
+    exit 1
+}
+
+#stop the service if it is running
+try
+{
+    write-host "Attempting to stop the service"
+    cmd --% /c sc stop MMBotService
+    #(get-wmiobject win32_service -filter "name='MMBotService'").stopService()
+}
+catch
+{
+    write-host "Unable to stop service, likely already stopped"
+}
+
+#archive the old scripts into a zip file in the root of c:\mmbot
+write-host "Creating an archive"
+Set-Location c:\mmbot
+Get-Childitem c:\mmbot\scripts -Recurse | Write-Zip -IncludeEmptyDirectories -OutputPath C:\mmbot\scriptsarchive.zip
+
+#delete all existing scripts
+write-host "Deleting all scripts on the target"
+Remove-Item c:\mmbot\scripts\*
+
+#copy over all the new scripts
+write-host "Copying over new scripts"
+Set-Location C:\mmbot-scripts
+Copy-Item c:\mmbot-scripts\scripts\* -Destination c:\mmbot\scripts
+
+#start the servic
+try
+{
+    write-host "Attempting to start the service"
+    cmd --% /c sc start MMBotService
+}
+catch
+{
+    write-host "Starting the service failed"
+}
