05a83f3676c52fcd09f7f069a4eaa5d307bdd9c6
Andrew Siemer <andrew@clear-measure.com>
2014-12-29 13:59:41 +0000

    asdf

--- build.ps1
+++ build.ps1
@@ -1,10 +1,9 @@
+$ErrorActionPreference = "Stop"
+
 $rootDir = "T:\TeamCity\buildAgent\work\b388614f6c9b6eba"
 $rootScriptsDir = $rootDir + "\scripts"
 $targetDir = "c:\mmbot"
 $targetScriptsDir = $targetDir + "\scripts"
-
-write-host $rootDir
-write-host $rootScriptDir
 
 #stop the service if it is running
 try
@@ -17,25 +16,34 @@ catch
     write-host "Unable to stop service, likely already stopped"
 }
 
+try
+{
+    #delete all existing scripts
+    write-host "Deleting all scripts on the target"
+    Set-Location $targetDir
+    Remove-Item $("$targetScriptsDir\*")
+    write-host $("$targetScriptsDir\*")
+}
+catch
+{
+    write-host "Deleting all the existing scripts failed"
+}
+try
+{
+    #copy over all the new scripts
+    write-host "Copying over new scripts"
+    Set-Location $rootDir
+    write-host "Current directory is " $rootDir
+    write-host "Copying from " $rootScriptsDir
+    write-host "to " $targetScriptsDir
+    Copy-Item $("$rootScriptsDir\*") -Destination $targetScriptsDir
+}
+catch
+{
+    write-host "Copying script files failed"
+}
 
-#delete all existing scripts
-write-host "Deleting all scripts on the target"
-Set-Location $targetDir
-#Remove-Item $("$targetScriptsDir\*")
-write-host $("$targetScriptsDir\*")
-
-#copy over all the new scripts
-write-host "Copying over new scripts"
-Set-Location $rootDir
-
-write-host "Current directory is " $rootDir
-write-host "Copying from " $rootScriptsDir
-write-host "to " $targetScriptsDir
-
-Copy-Item $("$rootScriptsDir\*") -Destination $targetScriptsDir
-write-host $("$rootScriptsDir\*") -Destination $targetScriptsDir
-
-#start the servic
+#start the service
 try
 {
     write-host "Attempting to start the service"
